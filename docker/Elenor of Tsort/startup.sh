4de6cb5f5a4022938e669c9407f7af06dee4ac6a
digitalduke <digitalduke@gmail.com>
2019-05-02 23:28:17 +0500

    Removed comments.

--- docker/Elenor of Tsort/startup.sh
+++ docker/Elenor of Tsort/startup.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
 set -e
-
-# dokuwiki_dir=/dokuwiki
-
-# if [ ! -d "$dokuwiki_dir" ]; then
-#     echo "DokuWiki does not appear to be installed correctly." >&2
-#     exit 1
-# fi
-
 chown -R nobody /var/www
-
 su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
-
 exec /usr/bin/supervisord -c /etc/supervisord.conf
