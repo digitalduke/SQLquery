a64a306a59708e3cb3b846d0a359ba9c9a8e1979
digitalduke <digitalduke@gmail.com>
2019-05-03 12:36:08 +0500

    Fixed group ownership.

--- docker/Elenor of Tsort/startup.sh
+++ docker/Elenor of Tsort/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -e
-chown -R nobody /var/www
+chown -R nobody:nobody /var/www
 su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
 exec /usr/bin/supervisord -c /etc/supervisord.conf
